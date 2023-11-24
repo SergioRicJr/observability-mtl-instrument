5e701fa90a3d9dd149bcf46efe74863e547e5fc1
SergioNascimento07 <sergioricjr7@gmail.com>
2023-11-24 20:01:04 -0300

    feat(metric_config): mapping of metric types changed from hashmap to ENUM class

--- observability_mtl_instrument/metric_config.py
+++ observability_mtl_instrument/metric_config.py
@@ -1,43 +1,53 @@
 from prometheus_client import Counter, CollectorRegistry, Summary, Gauge, Histogram, pushadd_to_gateway
 from prometheus_client.metrics import Metric
 from typing import Type
+from enum import Enum
+
+class MetricType(Enum):
+    COUNTER = 'counter'
+    SUMMARY = 'summary'
+    GAUGE = 'gauge'
+    HISTOGRAM = 'histogram'
+
+    @property
+    def metric_class(self) -> Type:
+        return {
+            MetricType.COUNTER: Counter,
+            MetricType.SUMMARY: Summary,
+            MetricType.GAUGE: Gauge,
+            MetricType.HISTOGRAM: Histogram
+        }[self]
 
 class MetricConfig:
     def __init__(self, job_name: str, prometheus_url: str) -> None:
         self.registry = CollectorRegistry()
         self.job_name = job_name
         self.prometheus_url = prometheus_url
-    
-        self.metrics_types: dict[str, Type[Metric]] = {
-            'counter': Counter,
-            'summary': Summary,
-            'gauge': Gauge,
-            'histogram': Histogram
-        }
 
         self.metrics = {}
 
         self.add_metric(
             title='http_requests_total_by_code',
-            type='counter',
+            type=MetricType.COUNTER,
             description='responses total by status code',
             labels=['http_code', 'unmapped', 'service']
         )
         self.add_metric(
             title='http_requests_duration_seconds',
-            type='summary',
+            type=MetricType.SUMMARY,
             description='reponse time of request',
             labels=['url_path', 'http_method', 'unmapped', 'service']
         )
         self.add_metric(
             title='requests_in_progress',
-            type='gauge',
+            type=MetricType.GAUGE,
             description='quantity of requests in progress',
             labels=['service']
         )
 
-    def add_metric(self, type: str, title: str, description: str, labels: list[str] = []):
-        self.metrics[title] = self.metrics_types[type](
+    def add_metric(self, type: MetricType, title: str, description: str, labels: list[str] = []):
+        metric_class = type.metric_class
+        self.metrics[title] = metric_class(
             title,
             description,
             labels,
